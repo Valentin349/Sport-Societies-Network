86fb8b6dc4d96c1b107d8d3d4a1eac4401d65eac
Drayz64 <65258846Drayz64@users.noreply.github.com>
2022-02-28 18:12:17 +0000

    Changed the frontend urls.py to work with React Router

--- spector/frontend/urls.py
+++ spector/frontend/urls.py
@@ -1,6 +1,10 @@
-from django.urls import path
+from django.urls import path, re_path
 from .views import index
 
 urlpatterns = [
+    # match the root
     path('', index),
+    # match all other pages using a regex path
+    re_path(r'^(?:.*)/?$', index),
 ]
+
