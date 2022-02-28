901b8d5a475f60114f7bbb09b72e72489bc33e87
Musa Qureshi <musaqureshi2002@gmail.com>
2022-02-28 14:50:45 +0000

    NavBar created without links

---
+++ spector/frontend/src/components/common/navbar/MenuItems.js
@@ -0,0 +1,13 @@
+export const MenuItems = [
+    {
+        title: 'Home',
+        url: '#',
+        cName: 'nav-links'
+    },
+    {
+        title: 'Sports',
+        url: '#',
+        cName: 'nav-links'
+    },
+
+]
