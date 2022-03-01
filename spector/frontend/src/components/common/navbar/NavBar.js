3645b0ab690f3b8eec53e2b7b887003a124b2f0b
Drayz64 <65258846Drayz64@users.noreply.github.com>
2022-03-01 18:52:53 +0000

    Implemented SPA with NavBar using <NavLink>

--- spector/frontend/src/components/common/navbar/NavBar.js
+++ spector/frontend/src/components/common/navbar/NavBar.js
@@ -1,39 +1,39 @@
-import React, { Component } from 'react';
-import { Button } from '../Button';
-import { MenuItems } from './MenuItems';
+import React from "react";
+import { NavLink } from "react-router-dom";
+import Button from "../Button";
+import { MenuItems } from "./MenuItems";
+// import "./NavBar.css";
 
-class NavBar extends Component {
-    state = { clicked: false }
+const NavBar = () => {
+  let activeStyle = {
+    borderRadius: "4px",
+    backgroundColor: "#2003ad",
+  };
 
+  return (
+    <nav className="NavBarItems">
+      <h1 className="navbar-logo">Spector</h1>
+      <ul className="nav-menu">
+        {MenuItems.map((item, index) => (
+          <li key={index}>
+            <NavLink
+              to={item.url}
+              className={item.cName}
+              //className={({ isActive }) =>
+              //  "nav-links" + (isActive ? " active" : "")
+              //}
+              // style={({ isActive }) => (isActive ? activeStyle : undefined)}
+              end
+            >
+              {item.title}
+            </NavLink>
+          </li>
+        ))}
+      </ul>
 
-
-    render() {
-        return(
-            <nav className='NavBarItems'>
-                <h1 className='navbar-logo'>Spector</h1>
-
-                
-                {/* <div className='menu-icon' onClick={this.handleClick}>
-                    <i className={this.
-                </div> */}
-
-                <ul className='nav-menu'>
-                    {MenuItems.map((item, index) => {
-                        return(
-                            <li key={index}>
-                                <a className={item.cName} href={item.url}>
-                                {item.title}
-                                </a>
-                            </li>
-                        )
-                    })}
-                </ul>
-
-                <Button>Profile</Button>
-
-            </nav>
-        )
-    }
-}
+      <Button>Profile</Button>
+    </nav>
+  );
+};
 
 export default NavBar;
