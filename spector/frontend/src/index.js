9f429ece33e279640e9474717fc1251bf0657ee0
Drayz64 <65258846Drayz64@users.noreply.github.com>
2022-03-01 18:53:27 +0000

    Made NavBar appear on any page and added an error page for invalid routes

--- spector/frontend/src/index.js
+++ spector/frontend/src/index.js
@@ -1,15 +1,25 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import NavBar from "./components/common/navbar/NavBar";
 import Home from "./routes/home";
 import Sports from "./routes/sports";
 
 // react renderer
 ReactDOM.render(
   <BrowserRouter>
+    <NavBar />
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="sports" element={<Sports />} />
+      <Route
+        path="*"
+        element={
+          <main style={{ padding: "1rem" }}>
+            <p>There&apos;s nothing here!</p>
+          </main>
+        }
+      />
     </Routes>
   </BrowserRouter>,
   document.getElementById("app")
