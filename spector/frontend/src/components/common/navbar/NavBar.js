e2dac87167cb640d5c56a65cc2c979a82eda05c3
Drayz64 <65258846Drayz64@users.noreply.github.com>
2022-03-01 23:21:31 +0000

    Moved the NavBar css code into a new css directory

--- spector/frontend/src/components/common/navbar/NavBar.js
+++ spector/frontend/src/components/common/navbar/NavBar.js
@@ -2,7 +2,9 @@ import React from "react";
 import { NavLink } from "react-router-dom";
 import Button from "../Button";
 import { MenuItems } from "./MenuItems";
-// import "./NavBar.css";
+//import style from "./NavBar.module.css";
+//import "./NavBar.css";
+import "css/components/NavBar.css";
 
 const NavBar = () => {
   let activeStyle = {
@@ -11,16 +13,20 @@ const NavBar = () => {
   };
 
   return (
-    <nav className="NavBarItems">
+    <nav className="NavBar-Items">
       <h1 className="navbar-logo">Spector</h1>
-      <ul className="nav-menu">
+      <ul className="NavBar-Menu">
         {MenuItems.map((item, index) => (
           <li key={index}>
             <NavLink
               to={item.url}
-              className={item.cName}
+              className="NavBar-Links"
+              //className={item.cName} // Doesn't work with css modules
               //className={({ isActive }) =>
               //  "nav-links" + (isActive ? " active" : "")
+              //}
+              //className={({ isActive }) =>
+              //  isActive ? `${style.active}` : `${style.navLinks}`
               //}
               // style={({ isActive }) => (isActive ? activeStyle : undefined)}
               end
