553ae39d98386be281e70a487c408f21d93dd33a
Drayz64 <65258846Drayz64@users.noreply.github.com>
2022-03-02 21:46:12 +0000

    Added loaders for css module incase we decide to use them

--- spector/frontend/webpack.config.js
+++ spector/frontend/webpack.config.js
@@ -25,7 +25,22 @@ module.exports = {
       },
       {
         test: /\.css$/,
+        use: [
+          "style-loader",
+          {
+            loader: "css-loader",
+            options: {
+              importLoaders: 1,
+              modules: true,
+            },
+          },
+        ],
+        include: /\.module\.css$/,
+      },
+      {
+        test: /\.css$/,
         use: ["style-loader", "css-loader"],
+        exclude: /\.module\.css$/,
       },
     ],
   },
