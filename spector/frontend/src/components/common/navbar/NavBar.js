92d3ae828cf8e4d8ddec9eaa6ce0b89578be3bba
Drayz64 <65258846Drayz64@users.noreply.github.com>
2022-03-02 03:06:00 +0000

    Cleaned up NavBar and changed cName in MenuItems.js

--- spector/frontend/src/components/common/navbar/NavBar.js
+++ spector/frontend/src/components/common/navbar/NavBar.js
@@ -2,41 +2,21 @@ import React from "react";
 import { NavLink } from "react-router-dom";
 import Button from "../Button";
 import { MenuItems } from "./MenuItems";
-//import style from "./NavBar.module.css";
-//import "./NavBar.css";
 import "css/components/NavBar.css";
 
 const NavBar = () => {
-  let activeStyle = {
-    borderRadius: "4px",
-    backgroundColor: "#2003ad",
-  };
-
   return (
     <nav className="NavBar-Items">
-      <h1 className="navbar-logo">Spector</h1>
+      <h1 className="navbar-logo">Spector</h1> {/*FIXME - where is the logo?*/}
       <ul className="NavBar-Menu">
         {MenuItems.map((item, index) => (
           <li key={index}>
-            <NavLink
-              to={item.url}
-              className="NavBar-Links"
-              //className={item.cName} // Doesn't work with css modules
-              //className={({ isActive }) =>
-              //  "nav-links" + (isActive ? " active" : "")
-              //}
-              //className={({ isActive }) =>
-              //  isActive ? `${style.active}` : `${style.navLinks}`
-              //}
-              // style={({ isActive }) => (isActive ? activeStyle : undefined)}
-              end
-            >
+            <NavLink to={item.url} className={item.cName} end>
               {item.title}
             </NavLink>
           </li>
         ))}
       </ul>
-
       <Button>Profile</Button>
     </nav>
   );
