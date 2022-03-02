8981179d46c63fe554720b4c6c8fe2666e6d64ba
Valentin349 <valentino7007@gmail.com>
2022-03-02 20:56:29 +0000

    added api filtering to activities endpoint

--- spector/spectorapp/views.py
+++ spector/spectorapp/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from rest_framework import mixins, viewsets
+from django_filters.rest_framework import DjangoFilterBackend
 from .models import UserData, Sports, Activity
 from .serializers import UserDataSerializer, SportSerializer, ActivitySerializer
 
@@ -8,9 +9,12 @@ class SportsViewSet(viewsets.GenericViewSet, mixins.ListModelMixin):
     queryset = Sports.objects.all()
     serializer_class = SportSerializer
 
-class ActivityViewSet(viewsets.ReadOnlyModelViewSet):
+class ActivityViewSet(viewsets.GenericViewSet, mixins.ListModelMixin):
     queryset = Activity.objects.all()
     serializer_class = ActivitySerializer
+    filter_backends = [DjangoFilterBackend]
+    filter_fields = ['sport', 'id']
+    
 
 class UserDataViewSet(viewsets.ModelViewSet):
     queryset = UserData.objects.all()
