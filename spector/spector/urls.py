25cc4ada0493ab7ba9ecb12fd5154a9bc482d108
Riley O'Keeffe <54857313+Rileyok@users.noreply.github.com>
2022-03-03 20:14:41 +0000

    Api token authorization/token generation implemented
    All API access can now be done with token authentication, basic (login popup) is still in place to browse api endpoints in browser. Tokens can be generated by post to /api-token-auth/ with valid username and password.

--- spector/spector/urls.py
+++ spector/spector/urls.py
@@ -15,9 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-
+from rest_framework.authtoken.views import obtain_auth_token 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('spectorapp.urls')),
-    path('', include('frontend.urls'))
+    path('', include('frontend.urls')),
+    path('api-token-auth/', obtain_auth_token, name='api_token_auth'),
 ]
