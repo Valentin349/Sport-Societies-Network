9ff93c23084c219a4487fef88f458b7161aa659e
Riley O'Keeffe <54857313+Rileyok@users.noreply.github.com>
2022-03-07 12:34:14 +0000

    User Serializer created, Authentication improved
    User serializer for user ownership of profile set up, Simple Auth removed, Session Auth added, Rest API Login implemented

--- spector/spectorapp/serializers.py
+++ spector/spectorapp/serializers.py
@@ -1,5 +1,16 @@
+from dataclasses import field
+import imp
+from pyexpat import model
 from rest_framework import serializers
 from .models import Profile, Sports, Activity
+from django.contrib.auth.models import User
+
+class UserSerializer(serializers.ModelSerializer):
+    userprofile = serializers.PrimaryKeyRelatedField(many=False, queryset=Profile.objects.all())
+
+    class Meta:
+        model = User
+        fields = ['id', 'username', 'userprofile']
 
 class ProfileSerializer(serializers.ModelSerializer):
     class Meta:
