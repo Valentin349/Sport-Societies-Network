f886b9d3b1920f37ae4b172e55ef644f0ee4cbd8
Riley O'Keeffe <54857313+Rileyok@users.noreply.github.com>
2022-03-07 15:22:34 +0000

    Added User ownership and permission for activity

--- spector/spectorapp/models.py
+++ spector/spectorapp/models.py
@@ -21,5 +21,6 @@ class Activity(models.Model):
     sport = models.ForeignKey(Sports, on_delete=models.CASCADE)
     name = models.CharField(max_length=30)
     description = models.CharField(max_length=200)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
     def __str__(self):
         return self.name
