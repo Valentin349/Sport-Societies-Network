aa9ed337dfd544244f68172b91fa6c5c203bc228
Valentin349 <valentino7007@gmail.com>
2022-03-09 17:44:04 +0000

    added user registration to backend

--- spector/spectorapp/views.py
+++ spector/spectorapp/views.py
@@ -1,9 +1,10 @@
-from django.shortcuts import render
-from rest_framework import mixins, viewsets
+from rest_framework import mixins, viewsets, permissions, status
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.authtoken.models import Token
 from django_filters.rest_framework import DjangoFilterBackend
 from .models import UserData, Sports, Activity
-from .serializers import UserDataSerializer, SportSerializer, ActivitySerializer
-from rest_framework import permissions
+from .serializers import UserDataSerializer, SportSerializer, ActivitySerializer, UserSerializer
 
 # Create your views here.
 class SportsViewSet(viewsets.GenericViewSet, mixins.ListModelMixin):
@@ -20,3 +21,22 @@ class UserDataViewSet(viewsets.ModelViewSet):
     queryset = UserData.objects.all()
     serializer_class = UserDataSerializer
     ##permission_classes = [permissions.IsAuthenticated]
+
+class CreateUserView(APIView):
+    """
+    Create/Register new user
+    """
+    # remove permissions for this view
+    permission_classes = []
+
+    def post(self, request, format='json'):
+        serializer = UserSerializer(data=request.data)
+        if serializer.is_valid():
+            user = serializer.save()
+            if user:
+                token = Token.objects.create(user=user)
+                json = serializer.data
+                json['token'] = token.key
+                return Response(json, status=status.HTTP_201_CREATED)
+        
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
