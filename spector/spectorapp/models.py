64eec61e90f83274ed2f83759ac694969a4637a5
Drayz64 <65258846Drayz64@users.noreply.github.com>
2022-03-11 14:29:17 +0000

    Testing storing and displaying dates to the user

--- spector/spectorapp/models.py
+++ spector/spectorapp/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.forms import CharField
+from django.forms import DateTimeField
+from django.utils import timezone
 
 # Create your models here.
 class UserData(models.Model):
@@ -18,5 +20,7 @@ class Activity(models.Model):
     sport = models.ForeignKey(Sports, on_delete=models.CASCADE)
     name = models.CharField(max_length=30)
     description = models.CharField(max_length=200)
+    dateTime = models.DateTimeField("Date & Time", default=timezone.now)
+    
     def __str__(self):
         return self.name
