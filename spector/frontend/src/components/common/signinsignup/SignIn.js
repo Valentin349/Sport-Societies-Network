c85790d2b587a610f2bd729cbe58f31f70ac8926
Musa Qureshi <musaqureshi2002@gmail.com>
2022-03-14 15:51:10 +0000

    Trial for sign in

--- spector/frontend/src/components/common/signinsignup/SignIn.js
+++ spector/frontend/src/components/common/signinsignup/SignIn.js
@@ -1,13 +1,47 @@
 import React, { useState } from "react";
 import "css/components/signIn.css";
+import useFetch from "../../../hooks/useFetch";
 
 const SignIn = () => {
+    const [error, setError] = useState(null);
+    const [data, setData] = useState([]);
     const [username, setUsername] = useState("");
     const [password, setPassword] = useState("");
+    const [isLoaded, setIsLoaded] = useState(false);
 
     const handleSubmit = (event) => {
         event.preventDefault();
-        alert(`The name you entered was: ${username} and password is: ${password}`)
+
+        // put req /api/login autenticate it with true/false 
+        const loginDetails = {username, password};
+        setIsLoaded(true);
+
+        // let headers = new Headers();
+        // headers.append("Content-Type", "application/json");
+
+        // fetch('/api/login/', {
+        //     method: "POST", 
+        //     headers: headers,
+        //     body: JSON.stringify(loginDetails)
+        // }).then((res) => res.json())
+        // .then(
+        //   (result) => {
+        //     setIsLoaded(true);
+        //     setData(result);
+        //   },
+        //   (error) => {
+        //     setIsLoaded(true);
+        //     setError(error);
+        //   }
+        // );
+        // if(!error){
+        //     console.log("works");
+        // }
+
+        useFetch('/api/login/', "POST", JSON.stringify(loginDetails));
+        
+            
+    
     }
 
     return (
