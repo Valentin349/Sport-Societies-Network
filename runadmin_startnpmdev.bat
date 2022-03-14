0525965a17268696b711aafaab3a7314b5e12bd1
jobron <80117666+jobron@users.noreply.github.com>
2022-03-14 14:50:41 +0000

    Launch server (Windows)
    run these as admin!!

---
+++ runadmin_startnpmdev.bat
@@ -0,0 +1,20 @@
+@setlocal enableextensions
+@cd /d "%~dp0"
+
+CALL .\webenv\scripts\activate.bat
+
+CALL pip install -r requirements.txt
+
+CALL cd spector
+
+CALL python manage.py makemigrations
+
+CALL python manage.py migrate
+
+CALL cd frontend
+
+CALL nvm use newest
+
+CALL npm ci
+
+CALL npm run dev
