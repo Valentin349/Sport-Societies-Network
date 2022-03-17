17a3b3b0915db6bda22695a4ea4051ba3c8726b5
valentin349 <valentino7007@gmail.com>
2022-03-17 14:35:23 +0000

    activity owner and members have both username and ids now

--- spector/spectorapp/models.py
+++ spector/spectorapp/models.py
@@ -11,7 +11,7 @@ class Profile(models.Model):
     name = models.CharField(max_length=30)
     bio = models.CharField(max_length=100)
     age = models.IntegerField(default=18)
-    owner = models.OneToOneField(User, on_delete=models.CASCADE)
+    owner = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True)
     def __str__(self):
         return self.name
 
--- spector/spectorapp/serializers.py
+++ spector/spectorapp/serializers.py
@@ -5,17 +5,15 @@ from rest_framework import serializers
 from .models import Profile, Sports, Activity
 from django.contrib.auth.models import User
 
+class MemberSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ('id', 'username')
+
 class ActivitySerializer(serializers.ModelSerializer):
-    members = serializers.SlugRelatedField(
-        many=True,
-        read_only=False,
-        queryset=User.objects.all(),
-        slug_field="username",
-    )
-    owner = serializers.SlugRelatedField(
-        read_only=True,
-        slug_field="username",
-    )
+    members = MemberSerializer(many=True)
+    owner = MemberSerializer()
+
     class Meta:
         model = Activity
         fields = '__all__'   
@@ -46,6 +44,7 @@ class UserSerializer(serializers.ModelSerializer):
     )
     username = serializers.CharField(
         max_length = 32,
+        required=True,
         validators=[UniqueValidator(queryset=User.objects.all())]
     )
     password = serializers.CharField(
