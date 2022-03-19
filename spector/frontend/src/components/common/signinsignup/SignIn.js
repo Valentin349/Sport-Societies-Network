fb5f778519f261b4cc12aff0e2b0999adac6ff67
Drayz64 <65258846Drayz64@users.noreply.github.com>
2022-03-19 17:11:40 +0000

    Changed how code gets into the catch statement

--- spector/frontend/src/components/common/signinsignup/SignIn.js
+++ spector/frontend/src/components/common/signinsignup/SignIn.js
@@ -5,8 +5,6 @@ import { useLocation, useNavigate } from "react-router-dom";
 const SignIn = () => {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
-  const [error, setError] = useState(null);
-  const [isLoaded, setIsLoaded] = useState(false);
   const [signInError, setSignInError] = useState("");
   const location = useLocation();
   const navigate = useNavigate();
@@ -24,34 +22,24 @@ const SignIn = () => {
     })
       .then((res) => {
         if (!res.ok) {
-          res.text().then(text => setSignInError(JSON.parse(text)));
+          return res.text().then((text) => {
+            throw new Error(text);
+          });
         }
-        //   if (res.status === 400) {
-        //     throw Error("Invalid credentials");
-        //   } else {
-        //     throw Error("Something went wrong");
-        //   }
-        // }
+
         return res.json();
       })
       .then((result) => {
         sessionStorage.setItem("token", result.token);
         sessionStorage.setItem("userID", result.id);
         sessionStorage.setItem("username", username);
-        setIsLoaded(true);
-        setError(null);
-
-        console.log("Token retrieved");
       })
       .then(() => {
         const prevPath = location.state?.from?.pathname || "/";
         navigate(prevPath);
       })
       .catch((error) => {
-        console.log(error.message);
-
-        setIsLoaded(true);
-        setError(error.message);
+        setSignInError(JSON.parse(error.message));
       });
   };
 
@@ -75,7 +63,7 @@ const SignIn = () => {
           value={password}
           onChange={(e) => setPassword(e.target.value)}
         />
-        <p className="signInError"> { signInError.non_field_errors }</p>
+        <p className="signInError">{signInError.non_field_errors}</p>
 
         <input type="submit" />
       </form>
