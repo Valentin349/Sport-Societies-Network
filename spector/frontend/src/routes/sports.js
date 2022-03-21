e36b24b73f66a516c547391a818efe57a1bc555f
jobron <80117666+jobron@users.noreply.github.com>
2022-03-21 15:07:34 +0000

    Fix styling and pass sport to popup
    this is definitely not correct :+1:

--- spector/frontend/src/routes/sports.js
+++ spector/frontend/src/routes/sports.js
@@ -94,7 +94,7 @@ const Sports = () => {
         <li>
           <ul className="socIndividualBox">
             Events...
-            <li><AddActivityPopup/></li>
+            <li><AddActivityPopup {...{sportName}}/></li>
             {data.map((item, index) => (
               <li key={index}>
                 <ActivityPopup
