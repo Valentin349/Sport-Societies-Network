da0e01582fe054081fd77f369bb386012fa530df
Riley O'Keeffe <54857313+Rileyok@users.noreply.github.com>
2022-03-22 11:35:28 +0000

    Event Signup Implemented
    Added Code to event signup button, joins events, toggles to leave, pressing leave leaves event. Checks on load if member, if so toggles to leave

--- spector/frontend/src/components/common/activityPopup.js
+++ spector/frontend/src/components/common/activityPopup.js
@@ -1,13 +1,84 @@
-import React from "react";
+import React, { useDebugValue } from "react";
 import Popup from "reactjs-popup";
 import "css/activityPopup.css";
 import PropTypes from "prop-types";
+import { useNavigate } from "react-router-dom";
+import { useEffect, useState } from "react";
 
 const ActivityPopup = (props) => {
   //props.dateTime is in the ISO format in UTC (Z)
   //So display the time in the users timezone (toLocale)
   //When posting to the api use ISO
+  const [isActive, setActive] = useState("false");
+  const [error, setError] = useState(null);
+  const [isLoaded, setIsLoaded] = useState(false);
+  const [data, setData] = useState([]);
 
+  const checkMember = () => {
+    let userId = sessionStorage.getItem("userID");
+    console.log(userId);
+    console.log(props.members);
+    if (props.members.includes(parseInt(userId))) {
+      setActive(false);
+    }
+  };
+
+  const JoinEvent = () => {
+    setActive(!isActive);
+
+    let userId = sessionStorage.getItem("userID");
+    let token = sessionStorage.getItem("token");
+    let headers = new Headers([
+      ["Content-Type", "application/json"],
+      ["Authorization", `Token ${token}`],
+    ]);
+    var body;
+    //get will get the members field to check for membership before writing to it
+    fetch(`/api/activities/${props.id}/`, {
+      method: "GET",
+      body: JSON.stringify(body),
+      headers: headers,
+    })
+      .then((res) => res.json())
+      .then(
+        (result) => {
+          setIsLoaded(true);
+          setData(result);
+          console.log(result.members);
+          let newMember = result.members;
+          if (result.members.includes(parseInt(userId))) {
+            newMember = newMember.filter((item) => item != parseInt(userId));
+          } else {
+            newMember.push(userId);
+          }
+
+          body = {
+            members: newMember,
+          };
+          console.log(body);
+          fetch(`/api/activities/${props.id}/`, {
+            method: "PATCH",
+            body: JSON.stringify(body),
+            headers: headers,
+          })
+            .then((res) => res.json())
+            .then(
+              (result) => {
+                setIsLoaded(true);
+                setData(result);
+              },
+              (error) => {
+                setIsLoaded(true);
+                setError(error);
+              }
+            );
+        },
+        (error) => {
+          setIsLoaded(true);
+          setError(error);
+        }
+      );
+  };
   return (
     <Popup
       trigger={
@@ -17,6 +88,7 @@ const ActivityPopup = (props) => {
           {props.duration}
         </button>
       }
+      onOpen={checkMember}
       modal
     >
       {(close) => (
@@ -46,7 +118,13 @@ const ActivityPopup = (props) => {
             {props.creationTime}
           </div>
           <div className="actions">
-            <button className="button">Join</button>
+            <button
+              className={isActive ? "button" : "buttonJoined"}
+              onClick={JoinEvent}
+              onLoad={checkMember}
+            >
+              {isActive ? "Join" : "Leave"}
+            </button>
           </div>
         </div>
       )}
@@ -63,6 +141,7 @@ ActivityPopup.propTypes = {
   maxMembers: PropTypes.number,
   owner: PropTypes.number,
   members: PropTypes.array,
+  id: PropTypes.number,
 };
 
 export default ActivityPopup;
