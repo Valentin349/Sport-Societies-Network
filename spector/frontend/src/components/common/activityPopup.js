15c8f50c7fbdb4d073d90322a6c2048c3109100c
Drayz64 <65258846Drayz64@users.noreply.github.com>
2022-03-23 02:58:28 +0000

    Moved handleMembership() to sport.js to avoid trying to change props value
    Not ideal to have isMember in ActivityPopup because if the fetch fails I can't stop join from switching

--- spector/frontend/src/components/common/activityPopup.js
+++ spector/frontend/src/components/common/activityPopup.js
@@ -5,88 +5,18 @@ import PropTypes from "prop-types";
 import { useState } from "react";
 
 const ActivityPopup = (props) => {
-  const [error, setError] = useState(null);
-  const [isLoaded, setIsLoaded] = useState(false);
-  const [data, setData] = useState([]);
-
   const userID = parseInt(sessionStorage.getItem("userID"));
-  const [isMember, setIsMember] = useState(() =>
-    props.members.includes(userID)
-  );
+  const [isMember, setIsMember] = useState(props.members.includes(userID));
 
-  const [isOwner, setIsOwner] = useState(
-    () => parseInt(props.owner) == parseInt(userID) || parseInt(userID) == 1
-  );
-
-  const HandleDelete = () => {
-    const token = sessionStorage.getItem("token");
-
-    let headers = new Headers([
-      ["Content-Type", "application/json"],
-      ["Authorization", `Token ${token}`],
-    ]);
-
-    if (isOwner) {
-      fetch(`/api/activities/${props.id}/`, {
-        method: "DELETE",
-        headers: headers,
-      })
-        .then((res) => res.text())
-        .then(
-          (result) => {
-            console.log(result ? JSON.parse(result) : "Success");
-          },
-          (error) => {
-            console.log(error);
-          }
-        );
-    }
-  };
-  const HandleClick = () => {
-    const token = sessionStorage.getItem("token");
-
-    let headers = new Headers([
-      ["Content-Type", "application/json"],
-      ["Authorization", `Token ${token}`],
-    ]);
-
-    let body;
-    if (isMember) {
-      body = { members: props.members.filter((id) => id != userID) };
-    } else {
-      props.members.push(userID);
-      body = { members: props.members };
-    }
-
-    fetch(`/api/activities/${props.id}/`, {
-      method: "PATCH",
-      body: JSON.stringify(body),
-      headers: headers,
-    })
-      .then((res) => res.json())
-      .then(
-        (result) => {
-          setIsLoaded(true);
-          setData(result);
-          setIsMember((isMember) => !isMember);
-        },
-        (error) => {
-          setIsLoaded(true);
-          setError(error);
-          console.log(error);
-        }
-      );
-
-    // Changing a state variable of sports.js to reload the component
-    // Means don't need to toggle isMember because setIsMember will be run on reload
-    props.reloadActivities({});
+  const HandleMembership = () => {
+    props.HandleMembership(isMember, props.index);
+    setIsMember((isMember) => !isMember);
   };
 
   return (
     <Popup
       trigger={
         <button className="button">
-          {/* {props.name} */}
           {props.name} || {new Date(props.startTime).toLocaleString()} ||{" "}
           {props.duration}
         </button>
@@ -122,8 +52,9 @@ const ActivityPopup = (props) => {
           </div>
           <div className="actions">
             <button
-              className={isMember ? "buttonJoined" : "button"}
-              onClick={HandleClick}
+              className={isMember ? "leaveButton" : "button"}
+              onClick={() => HandleMembership()}
+              disabled={props.members.length >= props.maxMembers && !isMember}
             >
               {isMember ? "Leave" : "Join"}
             </button>
@@ -153,7 +84,8 @@ ActivityPopup.propTypes = {
   duration: PropTypes.string,
   maxMembers: PropTypes.number,
   sport: PropTypes.string,
-  reloadActivities: PropTypes.func,
+  HandleMembership: PropTypes.func,
+  index: PropTypes.number,
 };
 
 export default ActivityPopup;
