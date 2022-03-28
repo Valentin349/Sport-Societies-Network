c424cc24cf014164a837b6e4ebe6015f2b1bf3bb
Drayz64 <65258846Drayz64@users.noreply.github.com>
2022-03-28 15:54:37 +0100

    Added methods to profile and home pages to handle joining and deleting

--- spector/frontend/src/routes/home.js
+++ spector/frontend/src/routes/home.js
@@ -1,10 +1,76 @@
-import React from "react";
+import React, { useState } from "react";
 import "css/home.css";
 import useFetch from "../hooks/useFetch";
 import ActivityPopup from "../components/common/activityPopup";
 
 const Home = () => {
+  const [, refresh] = useState();
+
   let userID = sessionStorage.getItem("userID");
+  const token = sessionStorage.getItem("token");
+  let headers = new Headers([
+    ["Content-Type", "application/json"],
+    ["Authorization", `Token ${token}`],
+  ]);
+
+  const HandleMembership = (isMember, index) => {
+    let activity = data[index];
+    const userID = parseInt(sessionStorage.getItem("userID"));
+
+    let headers = new Headers([
+      ["Content-Type", "application/json"],
+      ["Authorization", `Token ${token}`],
+    ]);
+
+    if (isMember) {
+      activity.members = activity.members.filter((id) => id != userID);
+    } else {
+      activity.members.push(userID);
+    }
+    let body = { members: activity.members };
+
+    fetch(`/api/activities/${activity.id}/`, {
+      method: "PATCH",
+      body: JSON.stringify(body),
+      headers: headers,
+    })
+      .then(async (res) => {
+        if (!res.ok) {
+          const text = await res.text();
+          throw new Error(text);
+        }
+
+        return res.json();
+      })
+      .then(() => {
+        refresh({});
+      })
+      .catch((error) => {
+        console.log(JSON.parse(error.message));
+        window.location.reload();
+      });
+  };
+
+  const HandleDelete = (isOwner, index) => {
+    let activity = data[index];
+
+    if (isOwner) {
+      fetch(`/api/activities/${activity.id}/`, {
+        method: "DELETE",
+        headers: headers,
+      })
+        .then((res) => res.text())
+        .then(
+          (result) => {
+            console.log(result ? JSON.parse(result) : "Success");
+            refresh({});
+          },
+          (error) => {
+            console.log(error);
+          }
+        );
+    }
+  };
 
   const { isLoaded, data, message } = useFetch(`/api/profile/${userID}/`);
 
@@ -21,7 +87,12 @@ const Home = () => {
           Joined Activities:
           {data.activities?.map((item, index) => (
             <li key={index}>
-              <ActivityPopup {...item} />
+              <ActivityPopup
+                HandleMembership={HandleMembership}
+                HandleDelete={HandleDelete}
+                index={index}
+                {...item}
+              />
             </li>
           ))}
         </ul>
--- spector/frontend/src/routes/profile.js
+++ spector/frontend/src/routes/profile.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { useParams } from "react-router-dom";
 import useFetch from "../hooks/useFetch";
 import "css/profile.css";
@@ -6,8 +6,75 @@ import ActivityPopup from "../components/common/activityPopup";
 
 const Profile = () => {
   let { userID } = useParams();
-
   const { isLoaded, data, message } = useFetch(`/api/profile/${userID}/`);
+
+  const [, refresh] = useState();
+
+  const token = sessionStorage.getItem("token");
+  let headers = new Headers([
+    ["Content-Type", "application/json"],
+    ["Authorization", `Token ${token}`],
+  ]);
+
+  const HandleMembership = (isMember, index) => {
+    let activity = data.activities[index];
+    console.log(activity);
+    const userID = parseInt(sessionStorage.getItem("userID"));
+
+    let headers = new Headers([
+      ["Content-Type", "application/json"],
+      ["Authorization", `Token ${token}`],
+    ]);
+
+    if (isMember) {
+      activity.members = activity.members.filter((id) => id != userID);
+    } else {
+      activity.members.push(userID);
+    }
+    let body = { members: activity.members };
+
+    fetch(`/api/activities/${activity.id}/`, {
+      method: "PATCH",
+      body: JSON.stringify(body),
+      headers: headers,
+    })
+      .then(async (res) => {
+        if (!res.ok) {
+          const text = await res.text();
+          throw new Error(text);
+        }
+
+        return res.json();
+      })
+      .then(() => {
+        refresh({});
+      })
+      .catch((error) => {
+        console.log(JSON.parse(error.message));
+        window.location.reload();
+      });
+  };
+
+  const HandleDelete = (isOwner, index) => {
+    let activity = data.activities[index];
+
+    if (isOwner) {
+      fetch(`/api/activities/${activity.id}/`, {
+        method: "DELETE",
+        headers: headers,
+      })
+        .then((res) => res.text())
+        .then(
+          (result) => {
+            console.log(result ? JSON.parse(result) : "Success");
+            refresh({});
+          },
+          (error) => {
+            console.log(error);
+          }
+        );
+    }
+  };
 
   if (!isLoaded) {
     return message;
@@ -30,7 +97,12 @@ const Profile = () => {
             Joined Activities:
             {data.activities?.map((item, index) => (
               <li key={index}>
-                <ActivityPopup {...item} />
+                <ActivityPopup
+                  HandleMembership={HandleMembership}
+                  HandleDelete={HandleDelete}
+                  index={index}
+                  {...item}
+                />
               </li>
             ))}
           </ul>
